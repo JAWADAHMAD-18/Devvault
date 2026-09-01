181db3481e6eebc7f9847e831b93ceac14c7ffc6
JAWADAHMAD-18 <jawadahmad5618@gmail.com>
2026-09-01 11:16:35 +0500

    Add middleware for token authentication on notes routes

---
+++ middleware.ts
@@ -0,0 +1,15 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(req: NextRequest) {
+  const token = req.cookies.get("token")?.value;
+
+  if (!token) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+
+  return NextResponse.next();
+}
+//jis route par chalna hy
+export const config = {
+  matcher: ["/notes/:path*"],
+};
